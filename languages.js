0a749b1e51b8eda90e4101fdde94fe89302bb97c
Mohammad Zakariya <mzakariya0007@gmail.com>
2026-01-11 21:42:21 +0530

    Create languages.js

---
+++ languages.js
@@ -0,0 +1,83 @@
+const translations = {
+  en: {
+    title: "Smart Aadhaar Center",
+    subtitle: "Center Monitoring System",
+    login: "Operator Login",
+    username: "Operator ID",
+    password: "Password",
+    button: "Login",
+    error: "Invalid credentials",
+    footer: "UIDAI Prototype | Hackathon Demo Designed & Developed by Mohammad Zakariya | mzakariya0007@gmail.com"
+
+  },
+  hi: {
+    title: "स्मार्ट आधार केंद्र",
+    subtitle: "केंद्र निगरानी प्रणाली",
+    login: "ऑपरेटर लॉगिन",
+    username: "ऑपरेटर आईडी",
+    password: "पासवर्ड",
+    button: "लॉगिन",
+    error: "गलत विवरण",
+    footer: "यूआईडीएआई प्रोटोटाइप   Designed & Developed by Mohammad Zakariya | mzakariya0007@gmail.com"
+  },
+  bn: {
+    title: "স্মার্ট আধার কেন্দ্র",
+    subtitle: "কেন্দ্র পর্যবেক্ষণ ব্যবস্থা",
+    login: "অপারেটর লগইন",
+    username: "অপারেটর আইডি",
+    password: "পাসওয়ার্ড",
+    button: "লগইন",
+    error: "ভুল তথ্য",
+    footer: "UIDAI প্রোটোটাইপ   Designed & Developed by Mohammad Zakariya | mzakariya0007@gmail.com "
+  },
+  mr: {
+    title: "स्मार्ट आधार केंद्र",
+    subtitle: "केंद्र निरीक्षण प्रणाली",
+    login: "ऑपरेटर लॉगिन",
+    username: "ऑपरेटर आयडी",
+    password: "पासवर्ड",
+    button: "लॉगिन",
+    error: "चुकीची माहिती",
+    footer: "UIDAI प्रोटोटाइप   Designed & Developed by Mohammad Zakariya | mzakariya0007@gmail.com"
+  },
+  te: {
+    title: "స్మార్ట్ ఆధార్ కేంద్రం",
+    subtitle: "కేంద్ర పర్యవేక్షణ వ్యవస్థ",
+    login: "ఆపరేటర్ లాగిన్",
+    username: "ఆపరేటర్ ఐడి",
+    password: "పాస్వర్డ్",
+    button: "లాగిన్",
+    error: "తప్పు వివరాలు",
+    footer: "UIDAI ప్రోటోటైప్ Designed & Developed by Mohammad Zakariya | mzakariya0007@gmail.com"
+  },
+  ta: {
+    title: "ஸ்மார்ட் ஆதார் மையம்",
+    subtitle: "மைய கண்காணிப்பு அமைப்பு",
+    login: "ஆபரேட்டர் உள்நுழைவு",
+    username: "ஆபரேட்டர் ஐடி",
+    password: "கடவுச்சொல்",
+    button: "உள்நுழை",
+    error: "தவறான விவரங்கள்",
+    footer: "UIDAI மாதிரி  Designed & Developed by Mohammad Zakariya | mzakariya0007@gmail.com"
+  },
+  ur: {
+    title: "اسمارٹ آدھار مرکز",
+    subtitle: "مرکزی نگرانی نظام",
+    login: "آپریٹر لاگ ان",
+    username: "آپریٹر آئی ڈی",
+    password: "پاس ورڈ",
+    button: "لاگ ان",
+    error: "غلط معلومات",
+    footer: "UIDAI پروٹوٹائپ  Designed & Developed by Mohammad Zakariya | mzakariya0007@gmail.com"
+  },
+  pa: {
+    title: "ਸਮਾਰਟ ਆਧਾਰ ਕੇਂਦਰ",
+    subtitle: "ਕੇਂਦਰ ਨਿਗਰਾਨੀ ਪ੍ਰਣਾਲੀ",
+    login: "ਓਪਰੇਟਰ ਲਾਗਇਨ",
+    username: "ਓਪਰੇਟਰ ਆਈਡੀ",
+    password: "ਪਾਸਵਰਡ",
+    button: "ਲਾਗਇਨ",
+    error: "ਗਲਤ ਜਾਣਕਾਰੀ",
+    footer: "UIDAI ਪ੍ਰੋਟੋਟਾਈਪ  Designed & Developed by Mohammad Zakariya | mzakariya0007@gmail.com"
+  }
+};
